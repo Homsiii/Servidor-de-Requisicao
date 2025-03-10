1505e14676283ce5215e12b71e46826717413fcc
Gabriel Homsi <gabriel.homsiga@gmail.com>
2025-03-10 13:26:40 -0300

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Servidor-de-Requisicao
+
+Edwin Nichollas Rocha Medina - 2221152
+Gabriel Homsi Gonçalves de Almeida - 2210364
+João Victor Guimarães - 2212057
+João Vitor Luiz - 2212349
---
+++ Servidor.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    login = data.get('login')
+    password = data.get('password')
+    
+    return jsonify({'login': login, 'password': password})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
